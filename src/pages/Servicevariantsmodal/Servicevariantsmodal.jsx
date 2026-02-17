95968866dbf492edd1796e221c71173d5481ea45
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-02-17 02:41:07 +0100

    dfdf df

--- src/pages/Servicevariantsmodal/Servicevariantsmodal.jsx
+++ src/pages/Servicevariantsmodal/Servicevariantsmodal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { X, Check, Clock, ShoppingCart, Plus } from "lucide-react";
-import styles from "./ServiceVariantsModal.module.css";
+import styles from "./Servicevariantsmodal.module.css";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
 
